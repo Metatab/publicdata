91ef49cb722d9ff6c5cd7fbc9f1aca6709f0e1e2
Eric Busboom <eric@civicknowledge.com>
2018-04-01 10:19:59 -0700

    rev

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if sys.argv[-1] == 'publish':
 
 setup(
     name='publicdata',
-    version='0.1.5',
+    version='0.1.6',
     url='https://github.com/CivicKnowledge/publicdata',
     license='MIT',
     author='Eric Busboom',
